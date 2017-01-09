bde49166a40827c2fe7fd08cde5a66a3a36fe701
Leighton Darkins <ldarkins@thoughtworks.com>
2017-01-09 17:47:18 +1000

    LD - injecting PrintStream rather than relying on System

--- src/com/leightondarkins/ConsoleApplication.java
+++ src/com/leightondarkins/ConsoleApplication.java
@@ -1,7 +1,15 @@
 package com.leightondarkins;
 
-public class ConsoleApplication {
-    public static void main(String[] args) {
+import java.io.PrintStream;
 
+public class ConsoleApplication {
+    private final PrintStream out;
+
+    public ConsoleApplication(PrintStream out) {
+        this.out = out;
+    }
+
+    public void WriteToConsole() {
+        out.print("test");
     }
 }
--- test/com/leightondarkins/ConsoleApplicationTest.java
+++ test/com/leightondarkins/ConsoleApplicationTest.java
@@ -1,12 +1,43 @@
 package com.leightondarkins;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ConsoleApplicationTest {
 
+    private ConsoleApplication subject;
+    private ByteArrayOutputStream outContent;
+    private PrintStream outStream;
+
+    @Before
+    public void setup() {
+        outContent = new ByteArrayOutputStream();
+        outStream = new PrintStream((outContent));
+        subject = new ConsoleApplication(outStream);
+    }
+
+    @After
+    public void tearDown() {
+        outContent = null;
+        outStream = null;
+        subject = null;
+    }
+
     @Test
     public void oneShouldEqualOne() {
         assertEquals(1,1);
+    }
+
+    @Test
+    public void WriteToConsole_ShouldWrite_Test() {
+        subject.WriteToConsole();
+
+        assertEquals("test", outContent.toString());
     }
 }
